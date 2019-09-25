d0aa4df60175f6a10ca6d679196ffc40a9c78b02
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-09-25 09:12:10 +0200

    Added artwork snapshot-test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
+import Artwork from '../components/Artwork/Artwork'
 
-it('renders correctly', () => {
+it('App renders correctly', () => {
   const tree = renderer
     .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Artwork renders correctly', () => {
+  const tree = renderer
+    .create(<Artwork artNr={1} picTheme={1} soundTheme={1} textTheme={1} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
