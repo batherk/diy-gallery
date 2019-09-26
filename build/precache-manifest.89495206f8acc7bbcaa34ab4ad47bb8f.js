fabac9f80dca1c003b829f1f2ec19fb17635eb52
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-09-26 10:23:29 +0200

    Feature/#9/deployment

---
+++ build/precache-manifest.89495206f8acc7bbcaa34ab4ad47bb8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb19294c9ba887147a96a8a27d9dc9a7",
+    "url": "/prosjekt2/index.html"
+  },
+  {
+    "revision": "4eb34e0450041aff1226",
+    "url": "/prosjekt2/static/css/main.3cee52ca.chunk.css"
+  },
+  {
+    "revision": "d95f8b624348dd14b5c1",
+    "url": "/prosjekt2/static/js/2.8c5061e8.chunk.js"
+  },
+  {
+    "revision": "4eb34e0450041aff1226",
+    "url": "/prosjekt2/static/js/main.cf5fb7e7.chunk.js"
+  },
+  {
+    "revision": "b91c4b4e156c61995330",
+    "url": "/prosjekt2/static/js/runtime~main.b663c3ee.js"
+  }
+]);
